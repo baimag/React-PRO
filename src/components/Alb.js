d85776ddc9d29bdac5433ef6c21ef36188b49805
baimag <baimag02@yandex.ru>
2020-12-15 15:10:56 +0300

    df

--- src/components/Alb.js
+++ src/components/Alb.js
@@ -3,13 +3,14 @@ import {selectAlbum} from "../redux/action";
 
 function Alb(props) {
     const dispatch = useDispatch()
+    const selectedAlbum = useSelector(state=> state.albums.selectedUserId)
     const nendleSelectedAlbum = () => {
         dispatch(selectAlbum(props.alb.id))
     }
 
 
     return(
-        <li onClick={nendleSelectedAlbum} className="Alb">
+        <li onClick={nendleSelectedAlbum} className={`Alb ${selectedAlbum === props.alb.id ? "active" : ""}`}>
            {props.alb.title}
         </li>
     )
--- src/redux/albums.js
+++ src/redux/albums.js
@@ -20,6 +20,11 @@ export default (state = initialState, action) => {
             ...state,
             selectedUserId: action.payload
         }
+        case 'filter/set':
+            return {
+                ...state,
+                filter: action.payload,
+            }
         default:
             return state
     }
